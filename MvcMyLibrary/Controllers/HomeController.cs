16df9d9f7e9f988fb563c1ae031869a84181bc92
Zenhun <sasa.zezelic@gmail.com>
2016-12-28 03:22:55 +0100

    - genre links css

--- MvcMyLibrary/Controllers/HomeController.cs
+++ MvcMyLibrary/Controllers/HomeController.cs
@@ -10,61 +10,18 @@ namespace MvcMyLibrary.Controllers
     public class HomeController : Controller
     {
         MyLibraryContext dbLibrary = new MyLibraryContext();
+        CompleteBook bookList = new CompleteBook();
+
         public ActionResult Index()
         {
-            //var books = dbLibrary.Books.ToList();
-            var books2 = (from b in dbLibrary.Books 
-                            join a in dbLibrary.Authors on b.AuthorId equals a.AuthorId 
-                            join g in dbLibrary.Genres on b.GenreId equals g.GenreId 
-                            select new {b.BookId,b.Title,a.Name,a.Surname, g.GenreName,b.ImageUrl}).ToList();
-            
-            //I made a class with all the elements of one book, gather from 3 tables: Books, Authors and Genres
-            List<CompleteBook> books = new List<CompleteBook>();
-
-            foreach (var book in books2)
-            {
-                var modelBook = new CompleteBook();
-                modelBook.BookId = book.BookId;
-                modelBook.Title = book.Title;
-                modelBook.AuthorName = book.Name;
-                modelBook.AuthorSurname = book.Surname;
-                modelBook.Genre = book.GenreName;
-                modelBook.CoverImageUrl = book.ImageUrl;
-
-                books.Add(modelBook);
-            }
-
-            //SELECT BookId,Title,a.Name,a.Surname,g.Name AS 'Genre',ImageUrl 
-            //FROM Book b 
-            //JOIN Author a ON b.AuthorID = a.AuthorID 
-            //JOIN Genre g ON b.GenreId = g.GenreId
+            List<CompleteBook> books = bookList.GetCompleteBooks();
 
             return View(books);
         }
 
         public ActionResult IndexGenre(int GenreId)
         {
-            var books2 = (from b in dbLibrary.Books
-                          join a in dbLibrary.Authors on b.AuthorId equals a.AuthorId
-                          join g in dbLibrary.Genres on b.GenreId equals g.GenreId
-                          where g.GenreId == GenreId
-                          select new { b.BookId, b.Title, a.Name, a.Surname, g.GenreName, b.ImageUrl }).ToList();
-            
-            //I made a class with all the elements of one book, gather from 3 tables: Books, Authors and Genres
-            List<CompleteBook> books = new List<CompleteBook>();
-
-            foreach (var book in books2)
-            {
-                var modelBook = new CompleteBook();
-                modelBook.BookId = book.BookId;
-                modelBook.Title = book.Title;
-                modelBook.AuthorName = book.Name;
-                modelBook.AuthorSurname = book.Surname;
-                modelBook.Genre = book.GenreName;
-                modelBook.CoverImageUrl = book.ImageUrl;
-
-                books.Add(modelBook);
-            }
+            List<CompleteBook> books = bookList.GetCompleteBooks(GenreId);
 
             return View("Index", books);
         }
@@ -73,7 +30,8 @@ namespace MvcMyLibrary.Controllers
         public ActionResult SideBar()
         {
             List<Genre> genres = (from g in dbLibrary.Genres
-                         select g).ToList();
+                                  orderby g.GenreName
+                                  select g).ToList();
 
             return PartialView(genres);
         }
--- MvcMyLibrary/Models/CompleteBook.cs
+++ MvcMyLibrary/Models/CompleteBook.cs
@@ -13,5 +13,48 @@ namespace MvcMyLibrary.Models
         public string AuthorSurname { get; set; }
         public string Genre { get; set; }
         public string CoverImageUrl { get; set; }
+
+        public List<CompleteBook> GetCompleteBooks()
+        {
+            return GetCompleteBooks(-1);
+        }
+
+        public List<CompleteBook> GetCompleteBooks(int GenreId)
+        {
+            MyLibraryContext dbLibrary = new MyLibraryContext();
+
+            var books2 = (from b in dbLibrary.Books
+                          join a in dbLibrary.Authors on b.AuthorId equals a.AuthorId
+                          join g in dbLibrary.Genres on b.GenreId equals g.GenreId
+                          select new { b.BookId, b.Title, a.Name, a.Surname, g.GenreName, b.ImageUrl }).ToList();
+
+            if (GenreId != -1)
+            {
+                books2 = (from b in dbLibrary.Books
+                          join a in dbLibrary.Authors on b.AuthorId equals a.AuthorId
+                          join g in dbLibrary.Genres on b.GenreId equals g.GenreId
+                          where g.GenreId == GenreId
+                          select new { b.BookId, b.Title, a.Name, a.Surname, g.GenreName, b.ImageUrl }).ToList();
+            }
+
+
+            //I made a class with all the elements of one book, gather from 3 tables: Books, Authors and Genres
+            List<CompleteBook> books = new List<CompleteBook>();
+
+            foreach (var book in books2)
+            {
+                var modelBook = new CompleteBook();
+                modelBook.BookId = book.BookId;
+                modelBook.Title = book.Title;
+                modelBook.AuthorName = book.Name;
+                modelBook.AuthorSurname = book.Surname;
+                modelBook.Genre = book.GenreName;
+                modelBook.CoverImageUrl = book.ImageUrl;
+
+                books.Add(modelBook);
+            }
+
+            return books;
+        }
     }
 }
