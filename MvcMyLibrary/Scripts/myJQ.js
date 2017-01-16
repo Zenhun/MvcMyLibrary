c39e36389d599bc4bdc9c14092ab07eec71cee9f
Zenhun <sasa.zezelic@gmail.com>
2017-01-16 22:52:14 +0100

    - last commit, ne znam šta sam radio :P

--- MvcMyLibrary/Scripts/myJQ.js
+++ MvcMyLibrary/Scripts/myJQ.js
@@ -8,23 +8,30 @@
     var $description = $("#description");
     var selected = 0;
 
+    //function to parse parameters from query string
+    function getUrlParameter(name) {
+        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        var results = regex.exec(location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    };
+
     $(".flex-item").not($(".book-icons")).click(function () {
         //don't change book info if clicked on the same book
         if (selected !== $(this).attr("id")) {
-
             $("#bookInfo").hide();
             $cover.html("");
             $description.text("");
             $subtitle.text("");
             $rating.text("");
 
-            $title.html($(this).find(".title").text());
-            $author.html($(this).find(".author").text());
+            $title.text($(this).children(".title").text());
+            $author.html($(this).children(".author").text());
             //$cover.append("<img src='" + $(this).find("img").attr("src") + "' />");
-            $genre.html($(this).find(".genre").text());
+            $genre.html($(this).children(".genre").text());
 
-            var bookSrc = "https://www.googleapis.com/books/v1/volumes?q=" + $title.text() + " " + $author.text() + "&maxResults=1";
-            //var bookSrc = "https://www.goodreads.com/search.xml?key=V6Iwmnm75zR91VuldBgZgw&q=" + $title.text() + " " + $author.text();
+            //var bookSrc = "https://www.googleapis.com/books/v1/volumes?q=" + $title.text() + " " + $author.text() + "&maxResults=1";
+            var bookSrc = "https://www.goodreads.com/search.xml?key=V6Iwmnm75zR91VuldBgZgw&q=" + $title.text() + " " + $author.text() + "&callback=?";
             //var bookSrc = "https://www.goodreads.com/search.xml?key=V6Iwmnm75zR91VuldBgZgw&q=" + $title.text() + " " + $author.text();
 
             //$.getJSON(bookSrc, function (data) {
@@ -38,7 +45,7 @@
             //});
 
             $.ajax(bookSrc, {
-                //dataType: "jsonp",
+                dataType: "jsonp",
                 //jsonpCallback: "success",
                 success: function (data) {
                     console.log(data);
@@ -53,6 +60,19 @@
 
             selected = $(this).attr("id");
         }
-        
     });
+
+    //bookInfo panel stays fixed vertically but moves horizontally if window gets narrower
+    // (I didn't use fixed position because bookInfo panel is taken out of the flow and if I shrink the window bookInfo panel covers the main book list)
+    $(window).scroll(function () {
+        $("#bookInfo").offset({ top: $(window).scrollTop() + 100});
+    });
+
+    //select active Genre side link
+    $(".genre-link.active").removeClass("active");
+    var activeGenreId = getUrlParameter("GenreId");
+    if (activeGenreId > 0) {
+        //find child of #sidebar ul element with id parameter equal to GenreId in query string
+        $("#sidebar ul").find("[id=" + activeGenreId + "]").addClass("active");
+    }
 });
