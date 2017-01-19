3c0c71c07af9cdd564ebe94dbde61dd0ffdef360
Zenhun <sasa.zezelic@gmail.com>
2017-01-20 00:49:45 +0100

    - SaveBook ongoing

--- MvcMyLibrary/Models/BookActions.cs
+++ MvcMyLibrary/Models/BookActions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -17,9 +18,18 @@ namespace MvcMyLibrary.Models
 
             MyLibraryContext dbLibrary = new MyLibraryContext();
 
-            SqlParameter paramName = new SqlParameter("@Name", authorName);
-            SqlParameter paramSurname = new SqlParameter("@Surname", authorSurname);
-            authorId = dbLibrary.Database.ExecuteSqlCommand("spCheckAuthor @Name, @Surname, @AuthorId", paramName, paramSurname, new SqlParameter("@AuthorId", DBNull.Value));
+            var returnCode = new SqlParameter();
+            returnCode.ParameterName = "@return_value";
+            returnCode.SqlDbType = SqlDbType.Int;
+            returnCode.Direction = ParameterDirection.Output;
+
+            //var outParam = new SqlParameter("@ReturnCode", SqlDbType.Int);
+            //outParam.Direction = ParameterDirection.Output;
+
+            SqlParameter paramName = new SqlParameter("@name", authorName);
+            SqlParameter paramSurname = new SqlParameter("@surname", authorSurname);
+            var data = dbLibrary.Database.SqlQuery<object>("exec @return_value = checkAuthor @name, @surname", returnCode, paramName, paramSurname);
+            authorId = (int)returnCode.Value;
             System.Diagnostics.Debug.WriteLine("Returned Author Id: " + authorId);
         }
     }
